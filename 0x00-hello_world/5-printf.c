c91ff70b24bbaca23344ec977f38d01e4e8cce76
Imad Naoum <imadnn159@gmail.com>
2023-03-17 04:12:17 -0700

    edit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
 * main - Entry point
-* 
+*
 * Return: Always 0 (Success)
 */
 int main(void)
@@ -10,3 +10,20 @@ int main(void)
 	return (0);
 }
 
+6-size.c
+
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
